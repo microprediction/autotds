cfe950bf18f7374807f21be2469c21c04c69fc13
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-04-19 09:36:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="autotds",
-    version="0.0.1",
+    version="0.0.2",
     description="Generate Towards Data Science (TDS) quality articles",
     long_description=README,
     long_description_content_type="text/markdown",
