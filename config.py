0349cc249c26bb6482f61bf405b05e698f97673d
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-04-19 09:18:19 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="autotds",
+    version="0.0.1",
+    description="Generate Towards Data Science (TDS) quality articles",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microprediction/autotds",
+    author="microprediction",
+    author_email="pcotton@intechinvestments.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["humpday", "humpday.objectives", "humpday.optimizers", "humpday.comparison", "humpday.visualization","humpday.transforms"],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    include_package_data=True,
+    install_requires=["wheel", "pathlib", "numpy>=1.19.5", "importlib-metadata>=1.7.0", "getjson"],
+    entry_points={
+        "console_scripts": [
+            "autotds=autotds.__main__:main",
+        ]
+    },
+)
